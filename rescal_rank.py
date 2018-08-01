c2215294df18dbd644b89f49401f07aaedf05970
LU YUXUN <yuxun-lu@h28is023.naist.jp>
2018-08-01 19:44:33 +0900

    entity index established.

--- rescal_rank.py
+++ rescal_rank.py
@@ -3,7 +3,8 @@ import numpy as np
 from scipy.sparse import lil_matrix
 from rescal import rescal_als
 import logging
-
+entitiy_index = {}
+rel_index = {}
 def read_data(filename):
     """
     Read relation / entities in freebase format.
@@ -20,6 +21,9 @@ def read_data(filename):
 
     entity_n = len(entity)
 
+    entity_index = {k:v for k,v in enumerate(entity)}
+
+
     data_table.columns = ["head", "rel", "tail"]
     # Packing it into a three-indices tensor.
     print("Packaging Tensors...")
@@ -27,10 +31,8 @@ def read_data(filename):
         tensor_slice = np.zeros([entity_n, entity_n])
         rel_dataFrame = data_table[data_table["rel"].str.contains(r)]
         for idx, row in rel_dataFrame.iterrows():
-            head_index = np.where( entity == row[0] )
-            head_index = head_index[0][0]
-            tail_index = np.where( entity == row[2])
-            tail_index = tail_index[0][0]
+            head_index = entity_index[row[0]]
+            tail_index = entity_index[row[2]]
             tensor_slice[head_index, tail_index] = 1
         tensor_slice = lil_matrix(tensor_slice)
         rel_mat.append(tensor_slice)
